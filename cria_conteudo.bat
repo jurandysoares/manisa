5bd37b99f6d3f9ca14a0e894fde85e7b7cfd741a
Jurandy Soares <jurandysoares@users.noreply.github.com>
2016-11-26 11:35:35 -0300

    Add files via upload

---
+++ cria_conteudo.bat
@@ -0,0 +1,90 @@
+echo > historico.rst
+
+echo > distribuicoes.rst
+
+echo > sistema.rst
+
+mkdir sistema
+
+cd sistema
+
+C:\Users\1577142\Documents\manisa\sistema>echo > shell.rst
+
+C:\Users\1577142\Documents\manisa\sistema>echo > nomes.rst
+
+C:\Users\1577142\Documents\manisa\sistema>echo > usuarios.rst
+
+C:\Users\1577142\Documents\manisa\sistema>echo > teclas.rst
+
+C:\Users\1577142\Documents\manisa\sistema>echo > gui.rst
+
+C:\Users\1577142\Documents\manisa\sistema>cd ..
+
+echo > instalacao.rst
+
+echo > discos.rst
+
+echo > memoria-virtual.rst
+
+echo > sistema-arquivos.rst
+
+mkdir sistema-arquivos
+
+cd sistema
+
+cd ..\sistema-arquivos
+
+echo > estrutura.rst
+
+echo > navegacao.rst
+
+cd ..
+
+echo > niveis-execucao.rst
+
+echo > id.rst
+
+echo > inicializacao.rst
+
+echo > pacotes.rst
+
+mkdir pacotes
+
+cd pacotes
+
+echo > apt.rst
+
+echo > dpkg.rst
+
+echo > rpm.rst
+
+echo > yum.rst
+
+cd ..
+
+echo > comandos.rst
+
+mkdir comandos
+
+cd comandos
+
+echo > chaves.rst
+
+echo > curinga.rst
+
+echo > ajuda.rst
+
+echo > aliases.rst
+
+echo segundo-plano.rst
+segundo-plano.rst
+
+echo > segundo-plano.rst
+
+echo > ger_diretorios.rst
+
+echo > ger_usuarios.rst
+
+echo > ger_sistema.rst
+
+echo > kernel.rst
